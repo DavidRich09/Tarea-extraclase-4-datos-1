faed25a17e92c3df854c82509c87037c107173e0
DavidRich09 <luis.richmond9@gmail.com>
2020-12-20 12:21:07 -0600

    interfaz

---
+++ app/src/main/java/com/example/lista/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.lista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText textIn;
+    private EditText textDel;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textIn = (EditText) findViewById(R.id.textIn);
+        textDel = (EditText) findViewById(R.id.textDel);
+        textView = (TextView) findViewById(R.id.viewNodes);
+    }
+
+    public void Insertar(View view){
+        String valor = textIn.getText().toString();
+        int numValor = Integer.parseInt(valor);
+    }
+
+    public void Eliminar(View view){
+        String valor = textDel.getText().toString();
+        int numValor = Integer.parseInt(valor);
+    }
+
+    public void Mostrar(String nodes){
+        textView.setText(nodes);
+    }
+
+}
